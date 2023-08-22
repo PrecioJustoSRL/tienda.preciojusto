9b1825c6c1780d02dc47dc4e70ac7400dba237b9
raulDeDevWorld <proyection009@gmail.com>
2023-08-22 17:26:44 -0400

    DC

--- src/components/QRscanner.jsx
+++ src/components/QRscanner.jsx
@@ -1,9 +1,12 @@
 import {QrScanner} from '@yudiel/react-qr-scanner';
+import { useUser } from '@/context/Context.js'
 
  const Component = () => {
+    const { user, userDB, cart, modal, setModal, productDB, setUserProduct, setUserItem, item, filter, setFilter, filterQR, setTienda, setFilterQR, recetaDBP, setRecetaDBP, tienda, setIntroClientVideo, search, setSearch, distributorPDB, setUserDistributorPDB, } = useUser()
+
   return (
       <QrScanner
-          onDecode={(result) => console.log(result)}
+          onDecode={(result) => setFilterQR(result)}
           onError={(error) => console.log(error?.message)}
       />
   );
