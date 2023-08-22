a31f881101379cebe88fd2d812f8bce0e3a32b98
raulDeDevWorld <proyection009@gmail.com>
2023-08-22 12:31:24 -0400

    DC

--- src/components/CardM.jsx
+++ src/components/CardM.jsx
@@ -61,7 +61,7 @@ export default function Card({ nombre1, nombre2, nombre3, costo, url, empresa, d
             <div>
                 <div class="relative w-[150px] rounded-t text-center" >
                     <img src={i.url} className='w-150px' alt="" />
-                    {recetado === true && <span className='absolute text-[16px] font-bold right-10 top-10 text-green-600 transform rotate-[-45deg]'>Recetado <br /> por tu doctor</span>}
+                    {recetado === true && <span className='absolute text-[16px] font-bold right-10 top-10 text-green-600 transform rotate-[-45deg]'>Recomendado <br /> por tu doctor</span>}
                 </div>
                 <div className='flex py-4 pr-4'>
                     <span className={`block text-center w-full text-14 p-2 rounded-[5px] text-[12px] leanding-[0px]`}>
