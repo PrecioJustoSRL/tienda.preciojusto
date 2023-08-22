479b03b4b71ca319173f1862a29bb44c03ff6293
raulDeDevWorld <proyection009@gmail.com>
2023-08-22 13:18:09 -0400

    DC

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,12 +9,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        <link rel="icon" href="/logo-dark.svg" />
+        <link rel="icon" href="/logo-circle.png" />
         <link rel='manifest' href='/manifest.json' />
-        <link rel='apple-touch-icon' href='/logo-dark.svg' />
+        <link rel='apple-touch-icon' href='/logo-circle.png' />
         <meta name="theme-color" content="#2A52BE" />
-        <meta name="msapplication-navbutton-color" content="#0064FA" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="#0064FA" />
+        <meta name="msapplication-navbutton-color" content="#2a52BE" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#2a52BE" />
         <meta name="description" content="Sistema de Administracion de Clientes y Formularios" />
         <meta name="keywords" content="Logistics Gear" />
         <meta name="author" content="Logistics Gear" />
