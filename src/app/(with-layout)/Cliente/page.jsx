1380b796679230fa3711202c0ef612649862d7c5
raulDeDevWorld <proyection009@gmail.com>
2023-09-07 01:47:55 -0400

    DC

--- src/app/(with-layout)/Cliente/page.jsx
+++ src/app/(with-layout)/Cliente/page.jsx
@@ -113,31 +113,9 @@ function Home() {
     // }
 
 
-    function isStandalone() {
-        return !!navigator.standalone || window.matchMedia('(display-mode: standalone)').matches;
-    }
-
-    // Depends on bowser but wouldn't be hard to use a
-    // different approach to identifying that we're running on Android
-    function exitsOnBack() {
-        return isStandalone() && browserInfo.os.name === 'Android';
-    }
-
-    // Everything below has to run at page start, probably onLoad
-
-    if (exitsOnBack()) handleBackEvents();
-
-    function handleBackEvents() {
-        window.history.pushState({}, '');
-
-        window.addEventListener('popstate', () => {
-            //TODO: Optionally show a "Press back again to exit" tooltip
-            setTimeout(() => {
-                window.history.pushState({}, '');
-                //TODO: Optionally hide tooltip
-            }, 2000);
-        });
-    }
+    window.addEventListener('load', function() {
+        window.history.pushState({}, '')
+      })
 
     // window.onbeforeunload =  function (e) {
     //     console.log(e)
