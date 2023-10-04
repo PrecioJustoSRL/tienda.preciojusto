4701df5f492185cdc6feac93b623ff1ca11f5f48
raulDeDevWorld <proyection009@gmail.com>
2023-10-03 21:56:10 -0400

    DC

--- src/components/WebCam.jsx
+++ src/components/WebCam.jsx
@@ -12,7 +12,10 @@ const CamScreen = () => {
     useEffect(() => {
         const enableVideoStream = async () => {
             try {
-                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                const stream = await navigator.mediaDevices.getUserMedia({ 
+                    video: {
+                        facingMode:'environment'
+                } });
                 setMediaStream(stream);
             } catch (error) {
                 console.error('Error accessing webcam', error);
@@ -21,6 +24,40 @@ const CamScreen = () => {
 
         enableVideoStream();
     }, []);
+
+
+
+
+    // function capture() {
+    //     var canvas = document.getElementById("canvas");
+    //     var video = document.querySelector("video");
+    //     canvas.width = video.videoWidth;
+    //     canvas.height = video.videoHeight;
+    //     canvas
+    //       .getContext("2d")
+    //       .drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+      
+    //     /** Code to merge image **/
+    //     /** For instance, if I want to merge a play image on center of existing image **/
+    //     const playImage = new Image();
+    //     playImage.src = "path to image asset";
+    //     playImage.onload = () => {
+    //       const startX = video.videoWidth / 2 - playImage.width / 2;
+    //       const startY = video.videoHeight / 2 - playImage.height / 2;
+    //       canvas
+    //         .getContext("2d")
+    //         .drawImage(playImage, startX, startY, playImage.width, playImage.height);
+    //       canvas.toBlob() = (blob) => {
+    //         const img = new Image();
+    //         img.src = window.URL.createObjectUrl(blob);
+    //       };
+    //     };
+    //     /** End **/
+    //   }
+      
+    //   capture();
+
+
 
 
 console.log(videoRef)
@@ -41,6 +78,12 @@ console.log(mediaStream)
             }
         };
     }, [mediaStream]);
+
+    useEffect(() => {
+        if (videoRef.current && mediaStream) {
+            videoRef.current.srcObject = mediaStream;
+        }
+    }, [videoRef, mediaStream]);
 
     return (
         <div>
